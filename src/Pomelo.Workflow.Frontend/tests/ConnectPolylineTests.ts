23da5d7522af3b14b4cc687d4481f691b3388fb2
Yuko Zheng <hi@yuko.me>
2022-04-05 22:22:03 +0800

    Update ConnectPolylineTests.ts

--- src/Pomelo.Workflow.Frontend/tests/ConnectPolylineTests.ts
+++ src/Pomelo.Workflow.Frontend/tests/ConnectPolylineTests.ts
@@ -1,7 +1,5 @@
 import assert = require('assert');
-import { ConnectPolyline } from '../ConnectPolyline';
 import { Drawing, DrawingConfiguration } from '../Drawing';
-import { Anchor, Shape } from '../Shape';
 
 describe("ConnectPolyline Tests", () => {
     it("Two Shapes Test", () => {
@@ -9,7 +7,7 @@ describe("ConnectPolyline Tests", () => {
         let drawing = new Drawing(new DrawingConfiguration());
         let shape1 = drawing.createShape(20, 20, 20, 20);
         let anchor1 = shape1.createAnchor(0, .5);
-        let shape2 = drawing.createShape(20, 20, 20, 20);
+        let shape2 = drawing.createShape(80, 100, 20, 20);
         let anchor2 = shape2.createAnchor(1, .5);
         let cp = drawing.createConnectPolyline(shape1.getGuid(), 0, shape2.getGuid(), 0);
         console.log('Shape 1:');
@@ -20,7 +18,7 @@ describe("ConnectPolyline Tests", () => {
         console.log(`Destination: (${shape2.getAnchors()[0].toPoint().x},${shape2.getAnchors()[0].toPoint().y})`);
 
         // Act
-        let ret = cp.initFromDepartureAndDestination(shape1.getAnchors()[0], shape2.getAnchors()[0]);
+        let ret = cp.getPathGenerationResult();
         console.log(cp.generateSvg());
 
         // Assert
@@ -31,91 +29,104 @@ describe("ConnectPolyline Tests", () => {
         assert.ok(ret, "Generate path should success");
     });
 
-    //it("Three Shapes Test #1", () => {
-    //    let cp = new ConnectPolyline();
-    //    let shape1 = new Shape(20, 20, 20, 20, null);
-    //    shape1.createAnchor(0, 0.5);
-    //    let shape2 = new Shape(80, 100, 20, 20, null);
-    //    shape2.createAnchor(1, 0.5);
-    //    let shape3 = new Shape(40, 80, 20, 20, null);
-    //    console.log('Shape 1:');
-    //    console.log(shape1.points);
-    //    console.log('Shape 2:');
-    //    console.log(shape2.points);
-    //    console.log(`Depatrue: (${shape1.anchors[0].toPoint().x},${shape1.anchors[0].toPoint().y})`);
-    //    console.log(`Destination: (${shape2.anchors[0].toPoint().x},${shape2.anchors[0].toPoint().y})`);
 
-    //    let ret = cp.initFromDepartureAndDestination(shape1.anchors[0], shape2.anchors[0], [shape1, shape2, shape3]);
-    //    console.log(cp.generateSvg());
+    it("Three Shapes Test #1", () => {
+        // Arrange
+        let drawing = new Drawing(new DrawingConfiguration());
 
-    //    let anchor1 = shape1.anchors[0].toPoint();
-    //    let anchor2 = shape2.anchors[0].toPoint();
-    //    assert.ok(ret, "This should success");
-    //});
+        let shape1 = drawing.createShape(20, 20, 20, 20);
+        let anchor1 = shape1.createAnchor(0, .5);
+        assert.equal(anchor1.toPoint().x, 20);
+        assert.equal(anchor1.toPoint().y, 20 + 10);
 
-    //it("Three Shapes Test #2", () => {
-    //    let cp = new ConnectPolyline();
-    //    let shape1 = new Shape(20, 20, 20, 20, null);
-    //    shape1.createAnchor(0, 0.5);
-    //    let shape2 = new Shape(80, 100, 20, 20, null);
-    //    shape2.createAnchor(1, 0.5);
-    //    let shape3 = new Shape(20, 40, 20, 20, null);
-    //    console.log('Shape 1:');
-    //    console.log(shape1.points);
-    //    console.log('Shape 2:');
-    //    console.log(shape2.points);
-    //    console.log(`Depatrue: (${shape1.anchors[0].toPoint().x},${shape1.anchors[0].toPoint().y})`);
-    //    console.log(`Destination: (${shape2.anchors[0].toPoint().x},${shape2.anchors[0].toPoint().y})`);
+        let shape2 = drawing.createShape(80, 100, 20, 20);
+        let anchor2 = shape2.createAnchor(1, .5);
+        assert.equal(anchor2.toPoint().x, 80 + 20);
+        assert.equal(anchor2.toPoint().y, 100 + 10);
 
-    //    let ret = cp.initFromDepartureAndDestination(shape1.anchors[0], shape2.anchors[0], [shape1, shape2, shape3]);
-    //    console.log(cp.generateSvg());
+        drawing.createShape(40, 80, 20, 20);
 
-    //    let anchor1 = shape1.anchors[0].toPoint();
-    //    let anchor2 = shape2.anchors[0].toPoint();
-    //    assert.equal(anchor1.x, 20);
-    //    assert.equal(anchor1.y, 20 + 10);
-    //    assert.equal(anchor2.x, 80 + 20);
-    //    assert.equal(anchor2.y, 100 + 10);
-    //    assert.ok(ret, "This should success");
-    //});
+        // Act
+        let cp = drawing.createConnectPolyline(shape1.getGuid(), 0, shape2.getGuid(), 0);
+        assert.equal(drawing.getShapes().length, 3);
+        console.log(cp.generateSvg());
 
-    //it("Three Shapes Test #3", () => {
-    //    let cp = new ConnectPolyline();
-    //    let shape1 = new Shape(20, 20, 20, 20, null);
-    //    shape1.createAnchor(0, 0.5);
-    //    let shape2 = new Shape(100, 20, 20, 20, null);
-    //    shape2.createAnchor(1, 0.5);
-    //    let shape3 = new Shape(50, 20, 20, 20, null);
-    //    console.log('Shape 1:');
-    //    console.log(shape1.points);
-    //    console.log('Shape 2:');
-    //    console.log(shape2.points);
-    //    console.log(`Depatrue: (${shape1.anchors[0].toPoint().x},${shape1.anchors[0].toPoint().y})`);
-    //    console.log(`Destination: (${shape2.anchors[0].toPoint().x},${shape2.anchors[0].toPoint().y})`);
 
-    //    let ret = cp.initFromDepartureAndDestination(shape1.anchors[0], shape2.anchors[0], [shape1, shape2, shape3]);
-    //    console.log(cp.generateSvg());
+        // Assert
+        assert.ok(cp.getPathGenerationResult(), "Generate path should success");
+    });
 
-    //    assert.ok(ret, "This should success");
-    //});
+    it("Three Shapes Test #2", () => {
+        // Arrange
+        let drawing = new Drawing(new DrawingConfiguration());
 
-    //it("Three Shapes Test #4", () => {
-    //    let cp = new ConnectPolyline();
-    //    let shape1 = new Shape(20, 20, 20, 20, null);
-    //    shape1.createAnchor(0, 0.5);
-    //    let shape2 = new Shape(100, 30, 20, 20, null);
-    //    shape2.createAnchor(0, 0.5);
-    //    let shape3 = new Shape(50, 20, 20, 20, null);
-    //    console.log('Shape 1:');
-    //    console.log(shape1.points);
-    //    console.log('Shape 2:');
-    //    console.log(shape2.points);
-    //    console.log(`Depatrue: (${shape1.anchors[0].toPoint().x},${shape1.anchors[0].toPoint().y})`);
-    //    console.log(`Destination: (${shape2.anchors[0].toPoint().x},${shape2.anchors[0].toPoint().y})`);
+        let shape1 = drawing.createShape(20, 20, 20, 20);
+        let anchor1 = shape1.createAnchor(0, .5);
+        assert.equal(anchor1.toPoint().x, 20);
+        assert.equal(anchor1.toPoint().y, 20 + 10);
 
-    //    let ret = cp.initFromDepartureAndDestination(shape1.anchors[0], shape2.anchors[0], [shape1, shape2, shape3]);
-    //    console.log(cp.generateSvg());
+        let shape2 = drawing.createShape(80, 100, 20, 20);
+        let anchor2 = shape2.createAnchor(1, .5);
+        assert.equal(anchor2.toPoint().x, 80 + 20);
+        assert.equal(anchor2.toPoint().y, 100 + 10);
 
-    //    assert.ok(ret, "This should success");
-    //});
+        drawing.createShape(20, 40, 20, 20);
+
+        // Act
+        let cp = drawing.createConnectPolyline(shape1.getGuid(), 0, shape2.getGuid(), 0);
+        assert.equal(drawing.getShapes().length, 3);
+        console.log(cp.generateSvg());
+
+
+        // Assert
+        assert.ok(cp.getPathGenerationResult(), "Generate path should success");
+    });
+
+    it("Three Shapes Test #3", () => {
+        // Arrange
+        let drawing = new Drawing(new DrawingConfiguration());
+
+        let shape1 = drawing.createShape(20, 20, 20, 20);
+        let anchor1 = shape1.createAnchor(0, .5);
+        assert.equal(anchor1.toPoint().x, 20);
+        assert.equal(anchor1.toPoint().y, 20 + 10);
+
+        let shape2 = drawing.createShape(100, 20, 20, 20);
+        shape2.createAnchor(1, .5);
+
+        drawing.createShape(50, 20, 20, 20);
+
+        // Act
+        let cp = drawing.createConnectPolyline(shape1.getGuid(), 0, shape2.getGuid(), 0);
+        assert.equal(drawing.getShapes().length, 3);
+        console.log(cp.generateSvg());
+
+
+        // Assert
+        assert.ok(cp.getPathGenerationResult(), "Generate path should success");
+    });
+
+    it("Three Shapes Test #4", () => {
+        // Arrange
+        let drawing = new Drawing(new DrawingConfiguration());
+
+        let shape1 = drawing.createShape(20, 20, 20, 20);
+        let anchor1 = shape1.createAnchor(0, .5);
+        assert.equal(anchor1.toPoint().x, 20);
+        assert.equal(anchor1.toPoint().y, 20 + 10);
+
+        let shape2 = drawing.createShape(100, 30, 20, 20);
+        shape2.createAnchor(1, .5);
+
+        drawing.createShape(50, 20, 20, 20);
+
+        // Act
+        let cp = drawing.createConnectPolyline(shape1.getGuid(), 0, shape2.getGuid(), 0);
+        assert.equal(drawing.getShapes().length, 3);
+        console.log(cp.generateSvg());
+
+
+        // Assert
+        assert.ok(cp.getPathGenerationResult(), "Generate path should success");
+    });
 });
